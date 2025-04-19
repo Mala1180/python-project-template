98dd0c3e2398ebc5b7e00988a22d30b9940b9961
Mattia Matteini <matteinimattia@gmail.com>
2025-04-19 12:44:48 +0200

    setup project

---
+++ test/test_math.py
@@ -0,0 +1,14 @@
+from src.math import sub, add, mul, div
+
+
+def test_sum():
+    assert add(1, 2) == 3
+
+def test_sub():
+    assert sub(2, 1) == 1
+
+def test_mul():
+    assert mul(2, 1) == 2
+
+def test_div():
+    assert div(6, 2) == 3
